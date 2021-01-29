eb72c4312a3c5337f80fff61da3e91d2f4646c69
mlodyFrontendowiec <pawel.kruszelnicki@onet.pl>
2021-01-29 20:13:46 +0100

    Add restart game

--- lib/screen/Game.dart
+++ lib/screen/Game.dart
@@ -3,12 +3,17 @@ import 'package:flutter/material.dart';
 class Game extends StatelessWidget {
   final List cards;
   final Function setCard;
-  Game(this.cards, this.setCard);
+  final Function resetGame;
+
+  Game(this.cards, this.setCard, this.resetGame);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: Text("Tic Tac Toe")),
+        appBar: AppBar(
+          title: Text("Tic Tac Toe"),
+          actions: [IconButton(icon: Icon(Icons.replay), onPressed: resetGame)],
+        ),
         body: GridView.builder(
             gridDelegate:
                 SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3),
